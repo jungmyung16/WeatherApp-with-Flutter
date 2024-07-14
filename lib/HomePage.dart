034ec32c9bcf84c719ec6920d7467180784ddf03
jungmyung <jungmyung30830@gmail.com>
2024-07-14 19:23:04 +0900

    메인 페이지

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
@@ -17,7 +18,7 @@ class _HomePageState extends State<HomePage> {
   String url = 'http://api.openweathermap.org/data/2.5/weather?'; // API 링크
   String lat = '';                                                // 위도
   String lon = '';                                                // 경도
-  String appId = 'appid=9375cf7ab1b655eb49960db7d5ebc132&';       // API 키
+  String appid = 'appid=9375cf7ab1b655eb49960db7d5ebc132&';       // API 키
   String units = 'units=metric';                                  // 데이터 반환 형태 변수
 
   void permission() async {
@@ -34,12 +35,24 @@ class _HomePageState extends State<HomePage> {
   /*
   날씨 정보를 가져오기 위해 위치 정보 획득
   desiredAccuracy: LocationAccuracy.high 데이터를 가장 높은 정확도로 받아온다
+
+  위치 정보와 정의된 정보들을 조합하여 날씨 정보 획득
+  인코딩된 링크 데이터 / 헤더 정보 전달
+  받은 데이터를 json형태로 weatherData에 저장한다
    */
   Future getData()async {
     Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
     print(position);
     lat = "lat=" + position.latitude.toString() + "&";
     lon = "lon=" + position.longitude.toString() + "&";
+
+    http.Response response = await http.get(
+      Uri.parse(url + lat + lon + appid + units),
+      headers:{"Accept":"application/json"},
+    );
+    print('Response body: ${response.body}');
+    weatherData = jsonDecode(response.body);
+    return weatherData;
   }
 
   /*
@@ -54,6 +67,12 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          getData();
+        },
+      ),
+    );
   }
 }
